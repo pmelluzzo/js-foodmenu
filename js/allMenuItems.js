7a23b248c1d9f2ee3112bc278aa1a91291bc824c
NineTwoProgramming <61386052+NineTwoProgramming@users.noreply.github.com>
2022-04-25 09:51:01 -0400

    Adding Cards
    Adding both cards to the HTML page and creating objects for the menu items

---
+++ js/allMenuItems.js
@@ -0,0 +1,36 @@
+
+const classicRolls = [
+  {name: "Asparagus Roll",
+  des: "8 pieces. Temp. asparagus, avocado, sesame seeds.",
+  price: "$5.78"},
+  {name: "Albacore Tuna Roll",
+  des: "8 pieces. Seared albacore tuna, avocado, green onion, sesame seeds.",
+  price: "$7.09"},
+  {name: "Spring Roll",
+  des: "4 pieces. Deep-fried vegetable roll served with sweet and sour sauce.",
+  price: "$6.48"}
+];
+
+const soupOfDay = [
+  {name: "Chicken Mushroom Soup",
+  des: "With goji berries, Chinese dates, ginger, and Shiitake mushrooms, this soup is healthy, if not downright healing–but also decidedly delicious!",
+  price: "$10.27"},
+  {name: "Lotus Root & Pork Soup",
+  des: "Very Good soup Very good",
+  price: "$9.09"},
+  {name: "Taiwanese Sesame Oil Chicken Soup",
+  des: "This Taiwanese sesame oil chicken soup will literally warm your bones. With loads of fresh ginger and rice wine, you’ll break a sweat by the time you’re at the bottom of your bowl. And by the way, have a side of rice with the chicken and make it into a full meal!",
+  price: "$6.48"},
+  {name: "Chicken Mushroom Soup",
+  des: "With goji berries, Chinese dates, ginger, and Shiitake mushrooms, this soup is healthy, if not downright healing–but also decidedly delicious!",
+  price: "$10.27"},
+  {name: "Lotus Root & Pork Soup",
+  des: "Very Good soup Very good",
+  price: "$9.09"},
+  {name: "Taiwanese Sesame Oil Chicken Soup",
+  des: "This Taiwanese sesame oil chicken soup will literally warm your bones. With loads of fresh ginger and rice wine, you’ll break a sweat by the time you’re at the bottom of your bowl. And by the way, have a side of rice with the chicken and make it into a full meal!",
+  price: "$6.48"},
+  {name: "Chicken Mushroom Soup",
+  des: "With goji berries, Chinese dates, ginger, and Shiitake mushrooms, this soup is healthy, if not downright healing–but also decidedly delicious!",
+  price: "$10.27"}
+];
--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 // Paul said it doesn't matter so here is a comment block
+function createMenuHTML(food){
+  return `<div class="col">
+    <div class="card" style="width: 18rem;">
+      <img src="https://place-hold.it/500" class="card-img-top" alt="">
+      <div class="card-body">
+        <h3>${soupOfDay[0].name}</h3>
+        <p class="card-text">${soupOfDay[0].des}</p>
+        <a href="#" class="btn btn-primary">View Nutrional Facts</a>
+      </div>
+    </div>
+  </div>`
+};
+
+document.getElementById('test').innerHTML = soupOfDay.map(food => createMenuHTML(food)).join('');
