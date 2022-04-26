722f6dc25b3b9d5b27d47fdcb77c64eb72068631
NineTwoProgramming <61386052+NineTwoProgramming@users.noreply.github.com>
2022-04-26 08:58:39 -0400

    Images and Descriptions
    Added images to the objects and added the correct descriptions for each soup.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ function createMenuHTML(food){
     return `
       <div class="col-3">
         <div class="card">
-          <img src="https://place-hold.it/500" class="card-img-top" alt="">
+          <img src="${food.img}" class="card-img-top" alt="">
           <div class="card-body">
             <h3>${food.name}</h3>
             <p class="card-text">${food.des}</p>
@@ -13,4 +13,4 @@ function createMenuHTML(food){
     `;
 };
 
-document.getElementById('menu-list').innerHTML = soupOfDay.map(food => createMenuHTML(food)).join('');
+document.getElementById('menu-list').innerHTML = menuItems.map(food => createMenuHTML(food)).join('');
