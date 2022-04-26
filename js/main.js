eaaf6df0d91570020a47f00359f32f719e028e71
pmelluzzo <paul.d.melluzzo@gmail.com>
2022-04-25 22:09:31 -0400

    JS Adjustments

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
-// Paul said it doesn't matter so here is a comment block
 function createMenuHTML(food){
-  return `<div class="col-3">
-    <div class="card" style="width: 18rem;">
-      <img src="https://place-hold.it/500" class="card-img-top" alt="">
-      <div class="card-body">
-        <h3>${soupOfDay[0].name}</h3>
-        <p class="card-text">${soupOfDay[0].des}</p>
-        <a href="#" class="btn btn-primary">View Nutrional Facts</a>
+    return `
+      <div class="col-3">
+        <div class="card">
+          <img src="https://place-hold.it/500" class="card-img-top" alt="">
+          <div class="card-body">
+            <h3>${food.name}</h3>
+            <p class="card-text">${food.des}</p>
+            <a href="#" class="btn btn-primary">View Nutrional Facts</a>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>`
+    `;
 };
 
 document.getElementById('menu-list').innerHTML = soupOfDay.map(food => createMenuHTML(food)).join('');
