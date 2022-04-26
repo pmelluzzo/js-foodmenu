aae6c25bea86dcf84bc54c76c90f3cf731b013e3
pmelluzzo <paul.d.melluzzo@gmail.com>
2022-04-26 16:08:02 -0400

    Added Modal CSS

--- js/allMenuItems.js
+++ js/allMenuItems.js
@@ -1,47 +1,88 @@
 
 const classicRolls = [
-  {name: "Asparagus Roll",
-  des: "8 pieces. Temp. asparagus, avocado, sesame seeds.",
-  price: "$5.78"},
-  {name: "Albacore Tuna Roll",
-  des: "8 pieces. Seared albacore tuna, avocado, green onion, sesame seeds.",
-  price: "$7.09"},
-  {name: "Spring Roll",
-  des: "4 pieces. Deep-fried vegetable roll served with sweet and sour sauce.",
-  price: "$6.48"}
+  {
+    name: "Asparagus Roll",
+    des: "8 pieces. Temp. asparagus, avocado, sesame seeds.",
+    price: "$5.78"
+  },
+  {
+    name: "Albacore Tuna Roll",
+    des: "8 pieces. Seared albacore tuna, avocado, green onion, sesame seeds.",
+    price: "$7.09"
+  },
+  {
+    name: "Spring Roll",
+    des: "4 pieces. Deep-fried vegetable roll served with sweet and sour sauce.",
+    price: "$6.48"
+  }
 ];
 
 const menuItems = [
-  {name: "Chicken Mushroom Soup",
-  img: "images/CCMS.jpg",
-  des: "With goji berries, Chinese dates, ginger, and Shiitake mushrooms, this soup is healthy, if not downright healing–but also decidedly delicious!",
-  price: "$10.27"},
-  {name: "Lotus Root & Pork Soup",
-  img: "images/LR&PS.jpg",
-  des: "Grandma’s classic! It has a light broth with fall apart pork simmered in a garlic and ginger infused homemade stock. As all Cantonese soups promise, one bowl is guaranteed to warm your soul!",
-  price: "$9.09"},
-  {name: "Taiwanese Sesame Oil Chicken Soup",
-  img: "images/SO&CS.jpg",
-  des: "This Taiwanese sesame oil chicken soup will literally warm your bones. With loads of fresh ginger and rice wine, you’ll break a sweat by the time you’re at the bottom of your bowl.",
-  price: "$6.48"},
-  {name: "Chicken Balls Soup",
-  img: "images/CBallS.jpg",
-  des: "This is just like everyone’s favorite cozy, comforting homemade chicken noodle soup except made even better with chicken meatballs! You’ll only want this version of chicken noodle soup after trying this! Promise!",
-  price: "$10.27"},
-  {name: "Pork Butt Soup",
-  img: "images/chinese-pork-stew-1.jpg",
-  des: "Pork Stew has tender chunks of pork and vegetables in a thick and hearty broth. It’s so flavorful and rich, you’ll have the whole family reaching for another bowl!",
-  price: "$9.09"},
-  {name: "Szechwan Hot Oil Hot Pot Soup",
-  img: "images/hot-pot-14-1.jpg",
-  des: "If you’re looking for a break from the traditional chili, corn chowder, or chicken noodle soup, you’ll want to try our Sichuan Hot Pot. This mouthwatering and customizable authentic soup will be made with anything you desire. ",
-  price: "$6.48"},
-  {name: "Mushroom Miso Soup",
-  img: "images/homemade-miso-soup-3.jpg",
-  des: "Miso is an ancient Japanese seasoning paste made from fermented soybeans. It’s got that salty, umami flavor that distinguishes so many Japanese dishes like this classic miso soup.",
-  price: "$10.27"},
-  {name: "Winter Melon Stew",
-  img: "images/220215_Winter-Melon-Soup-With-Meatballs_4.jpg",
-  des: "Winter melon soup is a delicately flavored yet warm, comforting dish that is usually served on cooler days. The winter melon is cooked in a rich-tasting chicken broth until tender. The addition of tiny tender homemade pork meatballs and fresh organic kale makes this simple soup taste delicious and hearty.",
-  price: "$10.27"}
+  {
+    name: "Chicken Mushroom Soup",
+    img: "images/CCMS.jpg",
+    des: "With goji berries, Chinese dates, ginger, and Shiitake mushrooms, this soup is healthy, if not downright healing–but also decidedly delicious!",
+    price: "$10.27",
+    nutrition: `
+                Nutrition Facts
+                Per 3/4 cup (175g)
+                Amount	% Daily Value
+                Calories 160
+                Fat 2.5 g	4 %
+                Saturated 1.5 g
+                + Trans 0 g	8 %
+                Cholesterol 10 mg
+                Sodium 75 mg	3 %
+                Carbohydrate 25 g	8 %
+                Fibre 0 g	0 %
+                Sugars 24 g
+                Protein 8 g
+                Vitamin A	2 %
+                Vitamin C	0 %
+                Calcium	20 %
+                Iron	0 %
+                `
+  },
+  {
+    name: "Lotus Root & Pork Soup",
+    img: "images/LR&PS.jpg",
+    des: "Grandma’s classic! It has a light broth with fall apart pork simmered in a garlic and ginger infused homemade stock. As all Cantonese soups promise, one bowl is guaranteed to warm your soul!",
+    price: "$9.09"
+  },
+  {
+    name: "Taiwanese Sesame Oil Chicken Soup",
+    img: "images/SO&CS.jpg",
+    des: "This Taiwanese sesame oil chicken soup will literally warm your bones. With loads of fresh ginger and rice wine, you’ll break a sweat by the time you’re at the bottom of your bowl.",
+    price: "$6.48"
+  },
+  {
+    name: "Chicken Balls Soup",
+    img: "images/CBallS.jpg",
+    des: "This is just like everyone’s favorite cozy, comforting homemade chicken noodle soup except made even better with chicken meatballs! You’ll only want this version of chicken noodle soup after trying this! Promise!",
+    price: "$10.27"
+  },
+  {
+    name: "Pork Butt Soup",
+    img: "images/chinese-pork-stew-1.jpg",
+    des: "Pork Stew has tender chunks of pork and vegetables in a thick and hearty broth. It’s so flavorful and rich, you’ll have the whole family reaching for another bowl!",
+    price: "$9.09"
+  },
+  {
+    name: "Szechwan Hot Oil Hot Pot Soup",
+    img: "images/hot-pot-14-1.jpg",
+    des: "If you’re looking for a break from the traditional chili, corn chowder, or chicken noodle soup, you’ll want to try our Sichuan Hot Pot. This mouthwatering and customizable authentic soup will be made with anything you desire. ",
+    price: "$6.48"
+  },
+  {
+    name: "Mushroom Miso Soup",
+    img: "images/homemade-miso-soup-3.jpg",
+    des: "Miso is an ancient Japanese seasoning paste made from fermented soybeans. It’s got that salty, umami flavor that distinguishes so many Japanese dishes like this classic miso soup.",
+    price: "$10.27"
+  },
+  {
+    name: "Winter Melon Stew",
+    img: "images/220215_Winter-Melon-Soup-With-Meatballs_4.jpg",
+    des: "Winter melon soup is a delicately flavored yet warm, comforting dish that is usually served on cooler days. The winter melon is cooked in a rich-tasting chicken broth until tender. The addition of tiny tender homemade pork meatballs and fresh organic kale makes this simple soup taste delicious and hearty.",
+    price: "$10.27"
+  }
 ];
--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ function createMenuHTML(food){
     return `
       <div class="col-3">
         <div class="card">
-          <img src="${food.img}" class="card-img-top" alt="">
+          <img src="${food.img}" class="card-img-top" alt="" data-modal="modal-one">
           <div class="card-body">
             <h3>${food.name}</h3>
             <p class="card-text">${food.des}</p>
-            <a href="#" class="btn btn-primary">View Nutrional Facts</a>
+            <a data-modal="modal-one" class="btn btn-primary">View Nutritional Facts</a>
           </div>
         </div>
       </div>
@@ -14,3 +14,20 @@ function createMenuHTML(food){
 };
 
 document.getElementById('menu-list').innerHTML = menuItems.map(food => createMenuHTML(food)).join('');
+
+const modals = document.querySelectorAll("[data-modal]");
+
+modals.forEach(function (trigger) {
+  trigger.addEventListener("click", function (event) {
+    event.preventDefault();
+    const modal = document.getElementById(trigger.dataset.modal);
+    modal.classList.add("open");
+    const exits = modal.querySelectorAll(".modal-exit");
+    exits.forEach(function (exit) {
+      exit.addEventListener("click", function (event) {
+        event.preventDefault();
+        modal.classList.remove("open");
+      });
+    });
+  });
+});
